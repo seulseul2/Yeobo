37da96cc5e7dae59f6e885ba700b9e9fdae4e9ee
조영훈 <gcloud1025@gmail.com>
2022-09-13 15:54:18 +0900

    [B]ADD_initial commit

--- backend/yeobo/yeobo/src/main/java/com/jagi/YeoboApplication.java
+++ backend/yeobo/yeobo/src/main/java/com/jagi/YeoboApplication.java
@@ -1,4 +1,4 @@
-package com.jagi.yeobo.yeobo;
+package com.jagi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/yeobo/yeobo/src/test/java/com/jagi/YeoboApplicationTests.java
+++ backend/yeobo/yeobo/src/test/java/com/jagi/YeoboApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jagi.yeobo.yeobo;
+package com.jagi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
