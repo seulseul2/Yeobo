20fc71178dc1dfe947048e216d022239d9f63fad
조영훈 <gcloud1025@gmail.com>
2022-09-13 15:31:26 +0900

    [B]ETC_initial commit

---
+++ backend/yeobo/yeobo/src/test/java/com/jagi/yeobo/yeobo/YeoboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jagi.yeobo.yeobo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeoboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
