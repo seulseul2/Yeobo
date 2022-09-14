84e835c130de83c806d4de40bd650f2e1abff55d
조영훈 <gcloud1025@gmail.com>
2022-09-14 10:21:09 +0900

    [B]ADD_mariadb connect

--- backend/yeobo/yeobo/src/main/java/com/jagi/yeobo/domain/User.java
+++ backend/yeobo/yeobo/src/main/java/com/jagi/yeobo/domain/User.java
@@ -1,4 +1,13 @@
 package com.jagi.yeobo.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 }
