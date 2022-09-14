62b3d3311070c61b0d7744f1041557f069138470
나장엽 <kaydenna92@gmail.com>
2022-09-14 11:25:15 +0900

    [F] ADD_library for MobileView

--- frontend/yeobo/src/App.js
+++ frontend/yeobo/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserView, MobileView } from 'react-device-detect';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserView>
+        데스크탑 뷰
+      </BrowserView>
+      <MobileView>
+        모바일 뷰
+      </MobileView>
     </div>
   );
 }
