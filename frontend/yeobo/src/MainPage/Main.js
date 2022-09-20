d6171361fee7a2ca94ad76d77c5470e6d634b981
mooyah5 <qorgkssk123@gmail.com>
2022-09-20 13:03:11 +0900

    [F] ADD_Publishing - MainRecom, MainBoddari

--- frontend/yeobo/src/MainPage/Main.js
+++ frontend/yeobo/src/MainPage/Main.js
@@ -1,11 +1,18 @@
 import './Main.scss';
 import MainTop from './MainTop';
+import MainRecom from './MainRecom';
+import MainBoddari from './MainBoddari';
 
 const Main = () => {
   return (
     <div className="Main back">
       {/* <h2>Main Page</h2> */}
       <MainTop />
+      <MainRecom />
+      <MainBoddari />
+      <div className='bottom'>
+        sdf
+      </div>
     </div>
   );
 };
