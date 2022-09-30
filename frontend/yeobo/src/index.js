9c7ecada9af81ce3616686ca7342c4ce683e1c65
mooyah5 <qorgkssk123@gmail.com>
2022-09-30 10:29:28 +0900

    [F] UPDATE_닉네임변경 모달, 마이페이지 관련 api

--- frontend/yeobo/src/index.js
+++ frontend/yeobo/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './assets/styles/index.scss';
 import App from './pages/App';
 import { BrowserRouter } from 'react-router-dom';
+import ReactModal from 'react-modal';
+
+ReactModal.setAppElement('#root');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- frontend/yeobo/src/pages/MyPage/AdminModule.js
+++ frontend/yeobo/src/pages/MyPage/AdminModule.js
@@ -1,12 +1,30 @@
 // import { Link } from 'react-router-dom';
 import "./AdminModule.scss";
+import { userDelete } from '../../api/user/userDelete';
+import { logout } from '../../api/user/logout';
+
+const userId = 16;
+
+function userDeleteClick() {
+  // const conf = confirm('정말?');
+  if (!!window.confirm('정말?')) {
+    alert('예')
+    userDelete(userId)
+  } else {
+    alert('이용하면 좋은 점 다시 한 번 어필하기..')
+  }
+}
+
+function logOutClick() {
+  logout();
+}
 
 const AdminModule = () => {
   return (
     <div>
       <div class="box">
-        <p>회원 탈퇴하기</p>
-        <p>로그아웃</p>
+        <p onClick={userDeleteClick}>회원 탈퇴하기</p>
+        <p onClick={logOutClick}>로그아웃</p>
       </div>
     </div>
     
