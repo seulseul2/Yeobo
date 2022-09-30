c2f3412cc01ff54b8b347ddd3ba85ba083f5ac16
junojam <nak3652@naver.com>
2022-09-30 13:35:55 +0900

    [D] ADD 4 API to jenkins

--- data/yeobo/views.py
+++ data/yeobo/views.py
@@ -62,21 +62,37 @@ def recommend(request, attraction_id):
 
 @api_view(['GET'])
 def area_code(request, attraction_id):
-    query_area_code = f"""
-    SELECT area_code
-    FROM attraction
-    WHERE attraction_id = {attraction_id}
-    """
+    
+    # # 1. attraction_id를 기준으로 area_code를 가져온다.
+    # query_area_code = f"""
+    # SELECT area_code
+    # FROM attraction
+    # WHERE attraction_id = {attraction_id}
+    # """
+    # # 2. query_mariaDB_category 함수를 통해 해당 area_code에 있는 목록을 가져온다.
+    # return Response(query_mariaDB_category(f"""
+    # SELECT *
+    # FROM attraction
+    # WHERE area_code = {query_mariaDB(query_area_code)['area_code'][0]} AND read_count >= 10000
+    # ORDER BY rand()
+    # LIMIT 30
+    # """), status=status.HTTP_200_OK)
+    
+    # 3. 아래는 위의 2 항목을 합친 반환값
     return Response(query_mariaDB_category(f"""
     SELECT *
     FROM attraction
-    WHERE area_code = {query_mariaDB(query_area_code)['area_code'][0]} AND read_count >= 10000
+    WHERE area_code = (SELECT area_code
+        FROM attraction
+        WHERE attraction_id = {attraction_id})
+    AND read_count >= 10000
     ORDER BY rand()
     LIMIT 30
     """), status=status.HTTP_200_OK)
     
 @api_view(['GET'])
 def pick_category(request, category):
+    print('바보')
     
     return Response(query_mariaDB_category(f"""
     SELECT *
@@ -93,6 +109,7 @@ def main_recommend(request, user_id):
     SELECT *
     FROM score
     """
+    user_attraction_score_matrix = query_mariaDB(query).pivot_table('score', index='attraction_id', columns='user_id')
     data = []
     attraction_main_recommend = int(query_mariaDB(f"""
     SELECT attraction_id
@@ -101,8 +118,9 @@ def main_recommend(request, user_id):
     ORDER BY score DESC, score_id DESC
     LIMIT 1
     """)['attraction_id'][0])
-    user_attraction_score_matrix = query_mariaDB(query).pivot_table('score', index='attraction_id', columns='user_id')
+    # print(attraction_main_recommend)
     attraction_sim = pd.DataFrame(cosine_similarity(user_attraction_score_matrix, user_attraction_score_matrix), index=user_attraction_score_matrix.index, columns=user_attraction_score_matrix.index)
+    # print(attraction_sim)
     lst = attraction_sim[attraction_main_recommend].sort_values(ascending=False)[0:5]
     for i in range(1, 5):
         data.append(query_mariaDB(f"""
