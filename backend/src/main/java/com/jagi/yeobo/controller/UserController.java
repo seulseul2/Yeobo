8fb603c7cdeb86b825dfc2a681a96353eab4bf24
leejieun <lgeun123@naver.com>
2022-10-02 22:55:55 +0900

    [B]UPDATE_cors

--- backend/src/main/java/com/jagi/yeobo/controller/UserController.java
+++ backend/src/main/java/com/jagi/yeobo/controller/UserController.java
@@ -19,7 +19,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.List;
 
-@CrossOrigin(originPatterns = "https://localhost:3000")
+@CrossOrigin(originPatterns = "https://localhost:3000, http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
 public class UserController {
