fa50916edea0d6d989a20fce05ac977f2fe0bebd
baekhannah <baekhannah@naver.com>
2022-10-05 02:51:51 +0900

    [F] ADD_로그인여부에 따라 내브바 변화(마이페이지/로그인)

--- frontend/yeobo/src/pages/BottomNavBar.js
+++ frontend/yeobo/src/pages/BottomNavBar.js
@@ -6,8 +6,11 @@ import user from "../assets/images/icons/user.png";
 import homeWhite from "../assets/images/icons/homewhite.png";
 // import homePink from '../assets/images/icons/home-pink.png';
 import search from "../assets/images/icons/searchicon.png";
+import { useSelector } from "react-redux";
 
 const BottomNavBar = () => {
+  const authenticated = useSelector((state) => state.authToken.authenticated);
+
   return (
     <div className="bottomnav">
       <div className="bottomBox">
@@ -35,11 +38,19 @@ const BottomNavBar = () => {
             <p className="iconText">합치기</p>
           </Link>
         </div>
+        {}
         <div className="iconTab">
-          <Link to="/Mypage">
-            <img className="homeIcon" src={user} alt="" />
-            <p className="iconText">마이페이지</p>
-          </Link>
+          {authenticated ? (
+            <Link to="/Mypage">
+              <img className="homeIcon" src={user} alt="" />
+              <p className="iconText">마이페이지</p>
+            </Link>
+          ) : (
+            <Link to="/Login">
+              <img className="homeIcon" src={user} alt="" />
+              <p className="iconText">로그인</p>
+            </Link>
+          )}
         </div>
       </div>
     </div>
