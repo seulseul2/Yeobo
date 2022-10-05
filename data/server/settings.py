3fcd4eb430cceeee538d6de4fbcd1650a4a7e436
junojam <nak3652@naver.com>
2022-10-05 14:10:21 +0900

    [D] FIX data API setting

--- data/server/settings.py
+++ data/server/settings.py
@@ -32,13 +32,13 @@ ALLOWED_HOSTS = ['j7c103.p.ssafy.io']
 
 INSTALLED_APPS = [
     'yeobo',
+    'corsheaders', # *
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders', # *
 ]
 
 MIDDLEWARE = [
