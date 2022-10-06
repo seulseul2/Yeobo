204d3eb70f756b4596e8cdb02a271cff6240c6b1
junojam <nak3652@naver.com>
2022-10-06 11:30:39 +0900

    [D] FIX attraction table score with attraction_score_cnt.csv to mariadb attraction_2

---
+++ data/dump_maker/dump_maker_7_csv_to_mariadb.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import pymysql
+
+def mysql_save(lst):
+    conn = pymysql.connect(
+        host = 'j7c103.p.ssafy.io',
+        port = 3307, 
+        user = 'yeobo', 
+        password = 'seulseul1004', 
+        db = 'yeobo', 
+        charset = 'utf8mb4'
+        )
+    cursor = conn.cursor()
+    # sql = "INSERT INTO ATTRACTION(attraction_id, score, cnt) values(%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s, %s)"
+    sql = "INSERT INTO attraction_2(attraction_id, score, cnt) values(%s, %s, %s)"
+    cursor.executemany(sql, lst)
+    conn.commit()
+    conn.close()
+    
+# df = pd.read_csv("attraction_score_4.csv")
+# df = df.drop(columns=['Unnamed: 0', 'category', 'name', 'description', 'address', 'areacode', 'image', 'image2', 'mapx', 'mapy', 'readcount'])
+# df.to_csv('attraction_score_cnt.csv')
+
+df = pd.read_csv('attraction_score_cnt.csv')
+df = df.drop(columns=['Unnamed: 0'])
+print(df)
+
+attraction_list = df.values.tolist()
+mysql_save(attraction_list)
